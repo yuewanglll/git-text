a59dcbec32098424f07c55c831140d691eb54968
HaoRan <HaoRan@123.com>
2024-12-18 12:58:16 +0800

    Two commit

--- src/main/java/com/yuewang/FirstGitDemo.java
+++ src/main/java/com/yuewang/FirstGitDemo.java
@@ -3,5 +3,6 @@ package com.yuewang;
 public class FirstGitDemo {
     public static void main(String[] args) {
         System.out.println("FirstGitDemo");
+
     }
 }
---
+++ src/main/java/com/yuewang/pojo/user.java
@@ -0,0 +1,17 @@
+package com.yuewang.pojo;
+
+
+
+public class user {
+
+private int id;
+private String name;
+
+    public user() {
+    }
+
+    public user(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
