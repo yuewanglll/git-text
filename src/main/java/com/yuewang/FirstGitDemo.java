6514c79f7ab367aeef73d601b40b6aa5bb0c1f82
HaoRan <HaoRan@123.com>
2024-12-18 13:15:33 +0800

    hot-fix commit1

--- src/main/java/com/yuewang/FirstGitDemo.java
+++ src/main/java/com/yuewang/FirstGitDemo.java
@@ -3,6 +3,8 @@ package com.yuewang;
 public class FirstGitDemo {
     public static void main(String[] args) {
         System.out.println("FirstGitDemo");
-
+        System.out.println("FirstGitDemo");
+        System.out.println("FirstGitDemo");
+        System.out.println("FirstGitDemo");
     }
 }
