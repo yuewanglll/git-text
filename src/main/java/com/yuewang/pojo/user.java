4affd6e8a83408411e090740c780684d75031df9
HaoRan <HaoRan@123.com>
2024-12-18 12:59:11 +0800

    thire commit

--- src/main/java/com/yuewang/pojo/user.java
+++ src/main/java/com/yuewang/pojo/user.java
@@ -14,4 +14,29 @@ private String name;
         this.id = id;
         this.name = name;
     }
+
+
+    @Override
+    public String toString() {
+        return "user{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
